6c7e147da0e4cced11ebde73a2f6f12ba2266d7b
NelsonGrajales <ngrajales2332@gmailcom>
2024-09-28 22:34:04 -0500

    primer cambio

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -2,8 +2,6 @@ return {
 	"tpope/vim-fugitive",
 	cmd = {"G","Git"},
 	keys = {{"<leader>ga", ":Git fetch --all -p<cr>", desc = "Git fetch"},
-		{"<leader>gl", "Git pull <cr>", desc = "Git pull "},
+		{"<leader>gl", "Git pull<cr>", desc = "Git pull"},
 	},
 }
-	
-

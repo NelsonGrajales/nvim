78d510c6f5f40514236e145a60048fb427d6c96e
NelsonGrajales <ngrajales2332@gmailcom>
2024-09-28 22:31:18 -0500

    usando fugitive

---
+++ init.lua
@@ -0,0 +1,7 @@
+require("config.lazy")
+
+vim.o.relativenumber = true
+vim.o.cursorline = true
+vim.o.tabstop = 4
+vim.opt.termguicolors = true
+vim.g.mapleader = " "
---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,10 @@
+return {
+    {
+        'wuelnerdotexe/vim-enfocado',
+        lazy = false,
+        priority = 1000,
+        config = function ()
+         vim.cmd.colorscheme 'enfocado'
+        end
+    }
+}
